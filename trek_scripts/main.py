e703a06d59d27733122f3a668468ac23714275bf
Michael Benfield <mike.benfield@gmail.com>
2018-06-07 10:21:31 -0700

    Better formatting for .txt files.

--- trek_scripts/main.py
+++ trek_scripts/main.py
@@ -53,36 +53,110 @@ def strip_html(directory):
     '''For each file .html file in this directory, create a .txt version
         with all html tags removed.'''
     path = pathlib.Path(directory)
-    regex_br = re.compile(r'<br>')
-    regex = re.compile(r'<.*?>')
-    regex2 = re.compile(r'^ +')
-    regex3 = re.compile(r' +$')
-    regex4 = re.compile(r'^\s*$')
-    regex_dialogue = re.compile(r"^[A-Z' \[\]]+:")
+    regex_br = re.compile(r'(?:<br>|</p>|<p>)')
+    regex_tag = re.compile(r'<.*?>')
+    regex_curly_square = re.compile(r'\{(.*?)\]')
+    regex_square_curly = re.compile(r'\[(.*?)\}')
+    regex_curly_paren = re.compile(r'\{(.*?)\)')
+    regex_paren_curly = re.compile(r'\(.*?\}')
+    regex_curly_curly = re.compile(r'\{.*?\}')
+    regex_leading_space = re.compile(r'^\s+')
+    regex_trailing_space = re.compile(r'\s+$')
+    regex_multiple_space = re.compile(r'  +')
+    regex_space_only = re.compile(r'^\s*$')
+
     for child in path.iterdir():
         if child.suffix == '.html':
             with open(child, encoding='Latin-1') as f:
                 lines = f.readlines()
-            # <br> tags to newlines
-            lines = [regex_br.sub('\n', line) for line in lines]
-            lines = ''.join(lines)
-            lines = lines.split('\n')
-            # remove html tags
-            lines = [regex.sub('', line) for line in lines]
-            # remove leading spaces
-            lines = [regex2.sub('', line) for line in lines]
-            # remove trailing spaces
-            lines = [regex3.sub('', line) for line in lines]
-            # remove double spaces
-            lines = [line.replace('  ', ' ') for line in lines]
-            # unescape HTML
-            lines = [html.unescape(line) for line in lines]
-            # remove empty lines
-            lines = [line for line in lines if
-                     regex4.match(line) is None]
 
-            # some odd formatting errors
-            lines = [line.replace('[OC}', '[OC]') for line in lines]
+            # remove newlines
+            lines = [line.replace('\n', ' ') for line in lines]
+
+            # concatenate
+            text = ''.join(lines)
+
+            # bogus tags
+            text = text.replace('&lt;br&gt;', '<br>')
+            text = text.replace('br&gt;', '<br>')
+
+            # <br> tags and </p> tags to newlines
+            text = regex_br.sub('\n', text)
+
+            # remove HTML tags
+            text = regex_tag.sub('', text)
+
+            # unescape HTML
+            text = html.unescape(text)
+
+            # from malformed tags
+            text = text.replace('\n>', '\n')
+
+            # correct braces
+            text = text.replace('[OC}', '[OC]')
+            text = text.replace('{OC]', '[OC]')
+            text = text.replace('{OC}', '[OC]')
+
+            text = regex_curly_square.sub('[\1]', text)
+            text = regex_square_curly.sub('[\1]', text)
+            text = regex_curly_curly.sub('[\1]', text)
+            text = regex_curly_paren.sub('(\1)', text)
+            text = regex_paren_curly.sub('(\1)', text)
+
+            # tabs
+            text = text.replace('\t', ' ')
+
+            # oddball characters
+            text = text.replace('\u0001', ' ')
+            text = text.replace('\u0091', ' ')
+            text = text.replace('\u0092', ' ')
+            text = text.replace('\u0096', ' ')
+            text = text.replace('\u00A0', ' ')
+
+            # multiple spaces
+            text = regex_multiple_space.sub(' ', text)
+
+            # typos
+            text = text.replace('Later -_', 'Later.)')
+            text = text.replace('_', ')')
+            text = text.replace('|', '')
+            text = text.replace('@', ':')
+            text = text.replace('>', '.')
+            text = text.replace('=', '-')
+
+            # more bogus tags
+            text = text.replace(' <', '')
+
+            # remove backticks
+            text = text.replace('`', '')
+
+            # remove hash
+            text = text.replace('#', '')
+
+            # undo some censorship
+            text = text.replace('****', 'bloody')
+
+            # non-Latin characters
+            text = text.replace('ß', 'ss')
+            text = text.replace('ä', 'a')
+            text = text.replace('à', 'a')
+            text = text.replace('ç', 'c')
+            text = text.replace('è', 'e')
+            text = text.replace('é', 'e')
+            text = text.replace('ê', 'e')
+            text = text.replace('ï', 'i')
+            text = text.replace('ó', 'o')
+
+            # Cyrillic text
+            text = text.replace('что случилось', "chto sluchilos'")
+
+            # remove empty lines
+            lines = text.split('\n')
+
+            lines = [line for line in lines if not regex_space_only.match(line)]
+            # remove leading spaces
+            lines = [regex_trailing_space.sub('', line) for line in lines]
+            lines = [regex_leading_space.sub('', line) for line in lines]
 
             # new title
             if lines[0].startswith('The Star Trek'):
@@ -96,28 +170,24 @@ def strip_html(directory):
             elif lines[0].startswith('The Enterprise'):
                 lines[0] = 'Star Trek: Enterprise'
 
-            # get rid of unnecessary newlines
+            # remove ending text
             lines0 = []
             for i in range(0, len(lines)):
                 line = lines[i]
-                if line.startswith('<Back'):
+                if line.startswith('<Back') or line.startswith('< Back'):
                     # transcript is over
                     break
-                if i <= 1 or line.startswith('[') or line.startswith('(') \
-                   or line.startswith('Original Airdate') \
-                   or regex_dialogue.match(line):
-                    # it should indeed be on its own line
-                    lines0.append(line)
-                else:
-                    # it should be appended to the previous line
-                    lines0[-1] += (' ' + line)
+                if line.startswith('Star Trek ®'):
+                    # transcript is over
+                    break
+                lines0.append(line)
             lines = lines0
 
-        new_file_name = child.with_suffix('.txt')
-        with open(new_file_name, 'w') as f:
-            for line in lines:
-                f.write(line)
-                f.write('\n')
+            new_file_name = child.with_suffix('.txt')
+            with open(new_file_name, 'w') as f:
+                for line in lines:
+                    f.write(line)
+                    f.write('\n')
 
 def arg_download(args):
     download(args.url, args.dir)
